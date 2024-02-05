0b461b48f7479a84a011df7573145db052f0f40b
garma00 <giovanniarmaleo00@gmail.com>
2024-02-05 17:43:32 +0100

    Add integration test

---
+++ __integrations__/__tests__/updateRCConfiguration.test.ts
@@ -0,0 +1,145 @@
+import * as TE from "fp-ts/lib/TaskEither";
+
+import { CosmosClient, CosmosClientOptions, Database } from "@azure/cosmos";
+import { pipe } from "fp-ts/lib/function";
+import { getNodeFetch } from "../utils/fetch";
+import {
+  REMOTE_CONTENT_COSMOSDB_KEY,
+  REMOTE_CONTENT_COSMOSDB_NAME,
+  REMOTE_CONTENT_COSMOSDB_URI
+} from "../env";
+import {
+  createRCCosmosDbAndCollections,
+  fillRCConfiguration
+} from "../__mocks__/fixtures";
+import { RCConfiguration } from "@pagopa/io-functions-commons/dist/src/models/rc_configuration";
+import { HasPreconditionEnum } from "@pagopa/io-functions-commons/dist/generated/definitions/HasPrecondition";
+import { NewRCConfiguration } from "@pagopa/io-functions-commons/dist/generated/definitions/NewRCConfiguration";
+import { NonEmptyString, Ulid } from "@pagopa/ts-commons/lib/strings";
+
+const baseUrl = "http://function:7071";
+
+const aDetailAuthentication = {
+  headerKeyName: "a" as NonEmptyString,
+  key: "key" as NonEmptyString,
+  type: "type" as NonEmptyString
+};
+
+export const aNewRemoteContentConfiguration: NewRCConfiguration = {
+  hasPrecondition: HasPreconditionEnum.ALWAYS,
+  disableLollipopFor: [],
+  isLollipopEnabled: false,
+  userId: "aUserId" as NonEmptyString,
+  name: "aRemoteContentConfiguration" as NonEmptyString,
+  description: "a description" as NonEmptyString,
+  prodEnvironment: {
+    baseUrl: "aValidUrl" as NonEmptyString,
+    detailsAuthentication: aDetailAuthentication
+  }
+};
+
+export const aRemoteContentConfiguration: RCConfiguration = {
+  hasPrecondition: HasPreconditionEnum.ALWAYS,
+  disableLollipopFor: [],
+  isLollipopEnabled: false,
+  userId: "aUserId" as NonEmptyString,
+  name: "aRemoteContentConfiguration" as NonEmptyString,
+  description: "a description" as NonEmptyString,
+  configurationId: "01HNG1XBMT8V6HWGF5T053K9RJ" as Ulid,
+  prodEnvironment: {
+    baseUrl: "aValidUrl" as NonEmptyString,
+    detailsAuthentication: aDetailAuthentication
+  }
+};
+
+export const aRemoteContentConfigurationList = [aRemoteContentConfiguration];
+
+const cosmosClient = new CosmosClient({
+  endpoint: REMOTE_CONTENT_COSMOSDB_URI,
+  key: REMOTE_CONTENT_COSMOSDB_KEY
+} as CosmosClientOptions);
+
+// eslint-disable-next-line functional/no-let
+let database: Database;
+
+beforeAll(async () => {
+  database = await pipe(
+    createRCCosmosDbAndCollections(cosmosClient, REMOTE_CONTENT_COSMOSDB_NAME),
+    TE.getOrElse(() => {
+      throw Error("Cannot create db");
+    })
+  )();
+  await fillRCConfiguration(database, aRemoteContentConfigurationList);
+});
+
+describe("UpdateRCConfiguration", () => {
+  test("should return a 400 error if the payload is not valid", async () => {
+    const aFetch = getNodeFetch({});
+    const body = {};
+    const r = await putCreateRCConfiguration(aFetch)(
+      body,
+      aRemoteContentConfiguration.configurationId
+    );
+    const jsonResponse = await r.json();
+
+    expect(r.status).toBe(400);
+    expect(jsonResponse.title).toBe("Invalid NewRCConfiguration");
+  });
+
+  test("should return a 400 error if the payload is valid but the configurationId is not", async () => {
+    const aFetch = getNodeFetch({});
+    const body = aNewRemoteContentConfiguration;
+    const r = await putCreateRCConfiguration(aFetch)(body, "invalidUlid");
+    const jsonResponse = await r.json();
+
+    expect(r.status).toBe(400);
+    expect(jsonResponse.title).toBe(
+      'Invalid string that matches the pattern "^[0-9a-hjkmnp-tv-zA-HJKMNP-TV-Z]{26}$"'
+    );
+  });
+
+  test("should return a 404 error if the payload and the configurationId are valid but the configuration does not exist", async () => {
+    const aFetch = getNodeFetch({});
+    const body = aNewRemoteContentConfiguration;
+    const nonExistingConfigurationId = "01HNX1RP85JYV9K96XK7GATWD1";
+    const r = await putCreateRCConfiguration(aFetch)(
+      body,
+      nonExistingConfigurationId
+    );
+    const jsonResponse = await r.json();
+
+    expect(r.status).toBe(404);
+    expect(jsonResponse.title).toBe("Configuration not found");
+    expect(jsonResponse.detail).toBe(
+      `Cannot find any remote-content configuration with id: ${nonExistingConfigurationId}`
+    );
+  });
+
+  test("should return a 204 no content if the request is ok and the configuration is correctly updated", async () => {
+    const aFetch = getNodeFetch({});
+    const body = aNewRemoteContentConfiguration;
+    const r = await putCreateRCConfiguration(aFetch)(
+      body,
+      aRemoteContentConfiguration.configurationId
+    );
+
+    expect(r.status).toBe(204);
+    // TODO: we can use the GetRCConfiguration to verify that the records were updated correctly once it is implemented
+  });
+});
+
+const putCreateRCConfiguration = (nodeFetch: typeof fetch) => async (
+  body: unknown,
+  configurationId: unknown
+) => {
+  return await nodeFetch(
+    `${baseUrl}/api/v1/remote-contents/configurations/${configurationId}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    }
+  );
+};
