63601066e6bf77b8f19a0088a3cf3c5f7ef5ac23
garma00 <giovanniarmaleo00@gmail.com>
2024-02-06 15:19:54 +0100

    Refactor middleware to be more functional

--- utils/middlewares.ts
+++ utils/middlewares.ts
@@ -1,4 +1,5 @@
 import * as E from "fp-ts/lib/Either";
+import * as T from "fp-ts/lib/Task";
 import { IRequestMiddleware } from "@pagopa/ts-commons/lib/request_middleware";
 import {
   IResponse,
@@ -6,15 +7,16 @@ import {
   ResponseErrorForbiddenAnonymousUser
 } from "@pagopa/ts-commons/lib/responses";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { pipe } from "fp-ts/lib/function";
 
 /**
  * A middleware that will extract the userId
  *
  * The middleware expects the header x-user-id
  */
-// TODO: refactor this miuddleware to be more functional
-// TODO: refactor this miuddleware to be more functional
-export const RequiredUserIdMiddleware = (): IRequestMiddleware<
+// TODO: refactor this middleware to be more functional
+// TODO: refactor this middleware to be generic
+export const RequiredUserIdMiddleware1 = (): IRequestMiddleware<
   "IResponseErrorForbiddenAnonymousUser",
   NonEmptyString
 > => (
@@ -26,17 +28,27 @@ export const RequiredUserIdMiddleware = (): IRequestMiddleware<
     // get Azure userId from the headers
     const errorOrUserId = NonEmptyString.decode(request.header("x-user-id"));
 
-    console.log(`request.header: ${request.header("x-user-id")}`);
-
     if (E.isLeft(errorOrUserId)) {
-      console.log(`Left: ${errorOrUserId.left}`);
       return resolve(E.left(ResponseErrorForbiddenAnonymousUser));
     }
-
-    console.log(`Right: ${errorOrUserId.right}`);
 
     const userId = errorOrUserId.right;
     resolve(
       E.right<IResponseErrorForbiddenAnonymousUser, NonEmptyString>(userId)
     );
   });
+
+export const RequiredUserIdMiddleware = (): IRequestMiddleware<
+  "IResponseErrorForbiddenAnonymousUser",
+  NonEmptyString
+> => (
+  request
+): Promise<
+  E.Either<IResponse<"IResponseErrorForbiddenAnonymousUser">, NonEmptyString>
+> =>
+  pipe(
+    request.header("x-user-id"),
+    NonEmptyString.decode,
+    E.mapLeft(() => ResponseErrorForbiddenAnonymousUser),
+    T.of
+  )();
