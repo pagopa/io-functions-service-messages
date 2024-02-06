5092cdc7e935eb2bd62906ca94280bae675c9eb8
garma00 <giovanniarmaleo00@gmail.com>
2024-02-06 14:24:31 +0100

    Fix handler

--- UpdateRCConfiguration/handler.ts
+++ UpdateRCConfiguration/handler.ts
@@ -9,21 +9,25 @@ import {
   wrapRequestHandler
 } from "@pagopa/io-functions-commons/dist/src/utils/request_middleware";
 import {
+  IResponseErrorForbiddenAnonymousUser,
   IResponseErrorInternal,
   IResponseErrorNotFound,
   IResponseSuccessNoContent,
+  ResponseErrorForbiddenAnonymousUser,
   ResponseErrorInternal,
   ResponseErrorNotFound,
   ResponseSuccessNoContent
 } from "@pagopa/ts-commons/lib/responses";
 import { pipe } from "fp-ts/lib/function";
 import { RCConfigurationModel } from "@pagopa/io-functions-commons/dist/src/models/rc_configuration";
-import { Ulid } from "@pagopa/ts-commons/lib/strings";
+import { NonEmptyString, Ulid } from "@pagopa/ts-commons/lib/strings";
 import { NewRCConfiguration } from "../generated/definitions/NewRCConfiguration";
+import { RequiredUserIdMiddleware } from "../utils/middlewares";
 
 interface IHandlerParameter {
   readonly configurationId: Ulid;
   readonly newRCConfiguration: NewRCConfiguration;
+  readonly userId: NonEmptyString;
 }
 
 interface IUpdateRCConfigurationHandlerParameter {
@@ -33,7 +37,10 @@ interface IUpdateRCConfigurationHandlerParameter {
 type UpdateRCConfigurationHandlerReturnType = (
   parameter: IHandlerParameter
 ) => Promise<
-  IResponseSuccessNoContent | IResponseErrorNotFound | IResponseErrorInternal
+  | IResponseSuccessNoContent
+  | IResponseErrorNotFound
+  | IResponseErrorInternal
+  | IResponseErrorForbiddenAnonymousUser
 >;
 
 type UpdateRCConfigurationHandler = (
@@ -43,7 +50,7 @@ type UpdateRCConfigurationHandler = (
 export const updateRCConfigurationHandler: UpdateRCConfigurationHandler = ({
   rccModel
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-}) => ({ newRCConfiguration, configurationId }) =>
+}) => ({ newRCConfiguration, configurationId, userId }) =>
   pipe(
     rccModel.findLastVersionByModelId([configurationId]),
     TE.mapLeft(e =>
@@ -59,9 +66,15 @@ export const updateRCConfigurationHandler: UpdateRCConfigurationHandler = ({
         )
       )
     ),
-    TE.chain(() =>
+    TE.chainW(
+      TE.fromPredicate(
+        configuration => configuration.userId === userId,
+        () => ResponseErrorForbiddenAnonymousUser
+      )
+    ),
+    TE.chainW(() =>
       pipe(
-        rccModel.upsert({ ...newRCConfiguration, configurationId }),
+        rccModel.upsert({ ...newRCConfiguration, configurationId, userId }),
         TE.mapLeft(e =>
           ResponseErrorInternal(
             `Something went wrong trying to upsert the configuration: ${e}`
@@ -92,13 +105,14 @@ export const getUpdateRCConfigurationExpressHandler: GetUpdateRCConfigurationHan
 
   const middlewaresWrap = withRequestMiddlewares(
     ContextMiddleware(),
+    RequiredUserIdMiddleware(),
     RequiredParamMiddleware("configurationId", Ulid),
     RequiredBodyPayloadMiddleware(NewRCConfiguration)
   );
 
   return wrapRequestHandler(
-    middlewaresWrap((_, configurationId, newRCConfiguration) =>
-      handler({ configurationId, newRCConfiguration })
+    middlewaresWrap((_, configurationId, userId, newRCConfiguration) =>
+      handler({ configurationId, newRCConfiguration, userId })
     )
   );
 };
